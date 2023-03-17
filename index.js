6047b3ec4c923556bdc1bcd3eb9c34ce9fe59c01
dev-rahim <rahimuddin5915@gmail.com>
2023-03-17 11:55:47 +0600

    places details updated

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.get('/', (req, res) => {
     res.send('Trip Trekkers Server Running')
 })
 
+// places data 
+const places = require('./data/places.json')
+app.get('/places', (req, res) => {
+    res.send(places)
+})
+
+
+
 app.listen(port, () => {
     console.log('Trip Trekkers Server Running on port=', port);
 })
